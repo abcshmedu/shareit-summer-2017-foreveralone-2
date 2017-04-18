ea822ff1b8835f2d46ba2cd024e78bd89e484359
Simon Weidacher <simon.weidacher@posteo.de>
2017-04-18 19:31:23 +0200

    Implements BookDao.

--- src/main/java/edu/hm/weidacher/softarch/shareit/data/dao/BookDao.java
+++ src/main/java/edu/hm/weidacher/softarch/shareit/data/dao/BookDao.java
@@ -1,7 +1,9 @@
 package edu.hm.weidacher.softarch.shareit.data.dao;
 
+import java.util.Collection;
 import java.util.UUID;
 
+import edu.hm.weidacher.softarch.shareit.data.database.DatabaseFactory;
 import edu.hm.weidacher.softarch.shareit.data.model.Book;
 import edu.hm.weidacher.softarch.shareit.exceptions.PersistenceException;
 
@@ -12,6 +14,12 @@ import edu.hm.weidacher.softarch.shareit.exceptions.PersistenceException;
  */
 public class BookDao implements UpdatableDao<Book> {
 
+    private final Collection<Book> database;
+
+    public BookDao() {
+        this.database = DatabaseFactory.getDatabase().getCollectionForType(Book.class);
+    }
+
     /**
      * Update an existing model.
      *
@@ -20,7 +28,20 @@ public class BookDao implements UpdatableDao<Book> {
      */
     @Override
     public void update(Book model) throws PersistenceException {
+        UUID id = model.getId();
+        if (id == null) {
+            throw new PersistenceException("Model has no id, can thus not be updated: " + model.toString());
+	}
 
+	// is a model present under the id?
+	final Book persisted = getById(id);
+
+	if (persisted == null) {
+            throw new PersistenceException("Not entity found for id, can thus not be updated: " + model.toString());
+	}
+
+	// book is present -> merge it!
+        persisted.mergeWith(model);
     }
 
     /**
@@ -31,7 +52,10 @@ public class BookDao implements UpdatableDao<Book> {
      */
     @Override
     public Book getById(UUID id) {
-	return null;
+	return database.stream()
+	    .filter(book -> id.equals(book.getId()))
+	    .findFirst()
+	    .orElse(null);
     }
 
     /**
@@ -42,7 +66,11 @@ public class BookDao implements UpdatableDao<Book> {
      */
     @Override
     public void store(Book model) throws PersistenceException {
+	if (getById(model.getId()) != null) {
+	    throw new PersistenceException("Book already present under the id!");
+	}
 
+	database.add(model);
     }
 
 }
